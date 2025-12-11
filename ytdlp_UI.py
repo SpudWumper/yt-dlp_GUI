2e063da9b123ed3665fe71611c1338ef5b5380a9
SpudWumper <rebornyuujinchou@gmail.com>
2025-12-11 01:51:30 -0600

    added url input and minimal button functionality

--- ytdlp_UI.py
+++ ytdlp_UI.py
@@ -1,4 +1,5 @@
-from yt_dlp import YoutubeDL
+import json
+import yt_dlp
 import tkinter as tk
 
 #class for the main window
@@ -7,3 +8,43 @@ class ytdlpGUI:
         self.window = tk.Tk()
         self.window.title("ytdlp gui")
         self.window.resizable(False,False)
+
+        sizeFrame = tk.Frame(width = w, height = h)
+        sizeFrame.pack()
+        top = tk.Frame(master=sizeFrame, width=w)
+        top.pack(fill=tk.X)
+
+        self.linkInLabel = tk.Label(master=top, text="Enter video URL here")
+        self.linkInLabel.pack(pady=5)
+        self.linkIn = tk.Entry(master=top,width=98)
+        self.linkIn.pack()
+        self.findVid = tk.Button(
+            master=top,
+            text="Load video",
+            width=10,
+            height=2,
+            relief="raised"
+        )
+        self.findVid.pack(pady=10)
+
+        bottom = tk.Frame(master=sizeFrame, width=w, height=h)
+        bottom.pack(side=tk.BOTTOM)
+
+    def loadVid(self, event):
+        ydl_opts = {
+            'listformats': True
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url=self.linkIn.get(), download=False)
+
+            #print(json.dumps(ydl.sanitize_info(info)))
+        
+
+    def enterMain(self):
+        self.findVid.bind("<Button-1>", self.loadVid)
+
+        self.window.mainloop()
+
+if __name__ == "__main__":
+    gui = ytdlpGUI(800, 600)
+    gui.enterMain()
