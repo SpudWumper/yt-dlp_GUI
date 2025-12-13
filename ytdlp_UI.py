42ff22d4b2487f83e3bb27b12d9e739ea0824dd3
SpudWumper <rebornyuujinchou@gmail.com>
2025-12-13 00:05:22 -0600

    added the rest of the download options added some quality of life like error checks and cleaning implemented download functionality
    things to work on:
     - stop the window from resizing
     - add timestamp options to download only section video

--- ytdlp_UI.py
+++ ytdlp_UI.py
@@ -1,7 +1,7 @@
 import json
 import yt_dlp
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, filedialog, messagebox
 import urllib.request
 from PIL import ImageTk, Image
 import io
@@ -57,17 +57,26 @@ class ytdlpGUI:
             text="Download video",
             width=16,
             height=2,
-            relief="raised"
+            relief="raised",
+            state=tk.DISABLED
         )
         self.downVid.pack(pady=10)
 
         
     # takes input URL and loads corresponding video and information
+    # - first clear the frames if we have already loaded a video
     # - extracts the info of video
     # - grabs thumbnail URL and converts to tkinter displayable image
     # - display title above thumbnail
     # - grab and display downloadable formats
+    # - input for file name and download location
+    # - checkbox for embed thumbnail option
     def loadVid(self, event):
+        for widget in self.vidInfoFrame.winfo_children():
+            widget.destroy()
+        for widget in self.formatOptFrame.winfo_children():
+            widget.destroy()
+
         ydl_opts = {
             
         }
@@ -95,6 +104,22 @@ class ytdlpGUI:
         imgLabel = tk.Label(master=self.vidInfoFrame, image=self.photo)
         imgLabel.pack()
 
+        filenameLabel = tk.Label(master=self.formatOptFrame, text="Enter filename here")
+        filenameLabel.pack()
+        self.filenameIn = tk.Entry(master=self.formatOptFrame, width=49)
+        self.filenameIn.pack(pady=5)
+        self.fileLocation = tk.Label(master=self.formatOptFrame, text="Download location: ", wraplength=396)
+        self.fileLocation.pack()
+        self.browse = tk.Button(
+            master=self.formatOptFrame,
+            text="Choose download location",
+            width=25,
+            height=2,
+            relief="raised"
+        )
+        self.browse.pack(pady=10)
+        self.browse.bind("<Button-1>", self.browseDir)
+
         formats = info.get('formats')
         audio = []
         video = []
@@ -108,17 +133,62 @@ class ytdlpGUI:
         audio.append('None')
         video.append('None')
         
-        audioCB = ttk.Combobox(master=self.formatOptFrame, values=audio, width=49)
-        audioCB.set('Choose an audio format')
-        audioCB.pack(pady=10)
-        videoCB = ttk.Combobox(master=self.formatOptFrame, values=video, width=49)
-        videoCB.set('Choose an video format')
-        videoCB.pack(pady=10)
+        self.audioCB = ttk.Combobox(master=self.formatOptFrame, values=audio, width=49)
+        self.audioCB.set('Choose an audio format')
+        self.audioCB.pack(pady=10)
+        self.videoCB = ttk.Combobox(master=self.formatOptFrame, values=video, width=49)
+        self.videoCB.set('Choose a video format')
+        self.videoCB.pack(pady=10)
 
-        
+        self.thumbnailYN = tk.IntVar()
+        self.embedThumbnail = tk.Checkbutton(master=self.formatOptFrame, text="Embed thumbnail", variable=self.thumbnailYN, onvalue=1, offvalue=0)
+        self.embedThumbnail.pack()
 
+        self.downVid.config(state=tk.NORMAL)
+
+    # function to handle file browsing, just a wrapper?
+    def browseDir(self, event):
+        dir = filedialog.askdirectory()
+        self.fileLocation.config(text=dir)
+
+    # downloads the video with selected options
+    def downloadVideo(self, event):
+        audioFormat = self.audioCB.get()
+        videoFormat = self.videoCB.get()
+        if audioFormat == 'Choose an audio format' or videoFormat == 'Choose a video format':
+            messagebox.showerror("Missing format selection", "Make sure to choose and audio and video format")
+        else:
+            audioCode = audioFormat.split()[0]
+            videoCode = videoFormat.split()[0]
+
+            if self.thumbnailYN.get() == 1:
+                ydl_opts = {
+                    'outtmpl': self.fileLocation.cget('text') + "\\" + self.filenameIn.get(),
+                    'format': audioCode+"+"+videoCode,
+                    'writethumbnail': True,
+                    'postprocessors': [
+                        {'key': 'FFmpegMetadata', 'add_metadata': True,},
+                        {'key': 'EmbedThumbnail'}
+                        ]
+                }
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    error_code = ydl.download(self.linkIn.get())
+                print("Video failed to download" if error_code
+                      else "Downloaded successfully")
+            else:
+                ydl_opts = {
+                    'outtmpl': self.fileLocation.cget('text') + "\\" + self.filenameIn.get(),
+                    'format': audioCode+"+"+videoCode
+                }
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    error_code = ydl.download(self.linkIn.get())
+                print("Video failed to download" if error_code
+                      else "Downloaded successfully")
+
+    # start up the gui
     def enterMain(self):
         self.findVid.bind("<Button-1>", self.loadVid)
+        self.downVid.bind("<Button-1>", self.downloadVideo)
 
         self.window.mainloop()
 
